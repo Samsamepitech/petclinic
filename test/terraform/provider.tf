f68a7e9fbdfd19e8810475a789eddb352f39ed3f
Samsam <samsamtaibi@gmail.com>
2023-07-10 14:06:37 +0200

    modif provider.tf

--- test/terraform/provider.tf
+++ test/terraform/provider.tf
@@ -1,20 +1,20 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google" }
-  }
-}
-provider "google" {
-  version = "4.51.0"
-  project = "taibi-samira"
-  credentials = ""
-  region  = "europe-west1"
-  zone    = "europe-west1-b"
-}
 # terraform {
-#   backend "gcs" {
-#     bucket      = "samsambucket"
-#     prefix      = "terraform"
-#     credentials =  ""
+#   required_providers {
+#     google = {
+#       source = "hashicorp/google" }
 #   }
 # }
+# provider "google" {
+#   version = "4.51.0"
+#   project = "taibi-samira"
+#   credentials = ""
+#   region  = "europe-west1"
+#   zone    = "europe-west1-b"
+# }
+# # terraform {
+# #   backend "gcs" {
+# #     bucket      = "samsambucket"
+# #     prefix      = "terraform"
+# #     credentials =  ""
+# #   }
+# # }
